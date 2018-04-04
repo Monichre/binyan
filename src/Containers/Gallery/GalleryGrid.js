dcc1b22dd070d1c97467712884a9831fa45a3b8c
Liam Ellis <liamhellis@gmail.com>
2018-04-04 19:15:20 -0400

    absolute grid basic code

--- src/Containers/Gallery/GalleryGrid.js
+++ src/Containers/Gallery/GalleryGrid.js
@@ -1,4 +1,17 @@
 import React, {Component} from 'react'
+import createAbsoluteGrid from 'AbsoluteGrid'
+
+class Grid extends Component {
+  render() {
+    return (
+      <div>
+        Grid
+      </div>
+    )
+  }
+}
+
+const AbsoluteGrid = createAbsoluteGrid(<Grid />, {...props})
 
 export default class GalleryGrid extends Component {
   constructor() {
